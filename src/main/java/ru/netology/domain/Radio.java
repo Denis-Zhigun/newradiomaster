4e234a600699ead8d59501f9243ec672e0457c8d
Denis Zhigun <zhigundv@gmail.com>
2021-03-11 20:58:15 +0300

    Project and tests updated

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -2,11 +2,11 @@ package ru.netology.domain;
 
 public class Radio {
     private int minStation = 0;
-    private int maxStation = 9;
+    private int maxStation = 10;
     private int currentStation;
     private int currentVolume;
     private int minVolume = 0;
-    private int maxVolume = 10;
+    private int maxVolume = 100;
 
     public int getCurrentStation() {
         return currentStation;
